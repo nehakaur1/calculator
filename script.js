9c6749051e35ad0f887e09f558bd194ccde92c4e
Neha <neahkaur816@gmail.com>
2024-06-07 15:01:52 +0530

    add String before eval

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ let arr = Array.from(buttons);
 arr.forEach((a) => {
   a.addEventListener("click", (e) => {
     if (e.target.innerHTML == "=") {
-      string = eval(string);
+      string = String(eval(string));
       input.value = string;
     } else if (e.target.innerHTML == "AC") {
       string = " ";
       input.value = string;
     } else if (e.target.innerHTML == "Del") {
-      string = string.substring(0, string.length- 1);
+      string = string.substring(0,string.length-1);
       input.value = string;
     } else {
       string += e.target.innerHTML;
